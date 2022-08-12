ddf707d4381d8875cf60124cee615f61eca1c644
Tor Andre Myrvoll <myrvoll@mac.com>
2022-08-12 21:54:26 +0200

    Changed Quark setup

--- keyboards/checkerboards/quark_plus/keymaps/miroboru/keymap.c
+++ keyboards/checkerboards/quark_plus/keymaps/miroboru/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     LCTL_T( KC_ESC),
                              KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
                     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                    MO(1),   MO(2),   KC_LGUI, KC_LALT, MO(3),   KC_LSFT, KC_SPC,  MO(4),   KC_RALT, KC_RGUI, KC_RCTL, KC_RSFT),
+                    MO(1),   MO(2),   KC_LALT, KC_LGUI, MO(3),   KC_LSFT, KC_SPC,  MO(4),   KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT),
 [1] = LAYOUT_grid(  KC_TRNS, KC_NO,   KC_NO,   KC_END,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_DEL,
                     KC_NO,   KC_HOME, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,
                     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGDN, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
